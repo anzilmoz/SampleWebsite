a5250ff79583e5420d02614c9964004957a74a68
anzilmoz <142879282+anzilmoz@users.noreply.github.com>
2024-09-09 13:30:46 +0530

    Update dummyCookie.js

--- js/dummyCookie.js
+++ js/dummyCookie.js
@@ -1,3 +1,3 @@
 const duration = new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000).toUTCString();
-document.cookie = "prism=prism_cookie_value; expires=" + duration;
-document.cookie = "prism=prism_cookie_value; domain=.datacurry.com; expires=" + duration;
+document.cookie = "prism=prism_cookie_value; path=/; expires=" + duration;
+// document.cookie = "prism=prism_cookie_value; domain=.datacurry.com; expires=" + duration;
